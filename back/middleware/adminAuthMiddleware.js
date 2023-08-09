98a5e7f8611393c484ccbb620678bd4bd6bdd9aa
Moksleivis <Moksleivis@kitm.local>
2023-08-09 09:25:20 +0300

    egzaminas1

---
+++ back/middleware/adminAuthMiddleware.js
@@ -0,0 +1,20 @@
+const asyncHandler = require('express-async-handler');
+const { getUser, notAuthorizedMessage } = require('./user');
+
+const protectAdmin = asyncHandler(async (req, res, next) => {
+    const { status, response } = await getUser(req);
+  
+    if (status === 200) {
+      if (response.role === 'admin') {
+        req.user = response;
+        next();
+      } else {
+        res.send(401, notAuthorizedMessage);
+      }
+    } else {
+      // res.send(status, response);
+      res.send(status).send(response);
+    }
+  });
+  
+  module.exports = { protectAdmin };
---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+  getUser,
+  deleteUser,
+} = require("../../controllers/userController");
+
+const { protectAdmin } = require("../../middleware/adminAuthMiddleware");
+
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/users", getUsers);
+router.get("/:id", getUser);
+router.route("/:id").delete(protectAdmin, deleteUser);
+
+module.exports = router;
